500331442e4d14c19e65fb6d31a471eb9deb7450
Raveler <Karel.Crombecq@gmail.com>
2013-01-25 22:36:20 +0100

    let's do this!

---
+++ scripts/Game.js
@@ -0,0 +1,143 @@
+define(["Compose", "Logger"],
+	function(Compose, Logger) {
+	
+	var Game = Compose(function constructor() {
+
+		// first time
+		this.firstTime = true;
+
+		// width, height
+		this.width = 1024;
+		this.height = 768;
+
+		// the canvas
+		this.canvas = document.createElement('canvas');
+		this.canvas.style = "canvas-game";
+		this.canvas.width = this.width;
+		this.canvas.height = this.height;
+
+		// Load images
+		var imagesFileNames=[];
+		//imagesFileNames.push("xx");
+		this.loadImages(imagesFileNames);
+
+		// Load json data
+		var jsonFileNames = [];
+		//jsonFileNames.push("bla");
+		this.loadJson(jsonFileNames);
+
+		// keys
+		this.keys = {};
+		this.keyDown = function(e) {
+			if (this.phase != 0) {
+				this.phase--;
+			}
+			var ch = String.fromCharCode(e.keyCode);
+			this.keys['key' + e.keyCode] = true;
+			if (e.keyCode != 116) e.preventDefault();
+		};
+		
+		this.keyUp = function(e) {
+			var ch = String.fromCharCode(e.keyCode);
+			this.keys['key' + e.keyCode] = false;
+			if (e.keyCode != 116) e.preventDefault();
+		};
+
+		this.mouseClick = function(e) {
+			if (this.phase != 0) {
+				this.phase--;
+			}
+			var mousePosX = -1;
+			var mousePosY = -1;
+			if (!e) var e = window.event;
+			if (e.pageX || e.pageY) 	{
+				mousePosX = e.pageX;
+				mousePosY = e.pageY;
+			}
+			else if (e.clientX || e.clientY) 	{
+				mousePosX = e.clientX + document.body.scrollLeft
+					+ document.documentElement.scrollLeft;
+				mousePosY = e.clientY + document.body.scrollTop
+					+ document.documentElement.scrollTop;
+			}
+
+			if ((this.mousePosX != -1) && (this.mousePosX != -1)) {
+				this.mousePressed = true;
+				mousePosX -= this.canvas.offsetLeft;
+				mousePosY -= this.canvas.offsetTop;
+
+				this.MousePosition = new Vector2(mousePosX, mousePosY);
+			}
+		};
+		
+		document.onkeydown = this.keyDown.bind(this);
+		document.onkeyup = this.keyUp.bind(this);
+		this.canvas.onmousedown = this.mouseClick.bind(this);
+	},
+	{
+		getImage: function(name) {
+			return this.images[name];
+		},
+
+		update: function() {
+
+			if (this.gameOver) {
+				this.drawGameOver();
+				return;
+			}
+
+			if (!(this.imagesPending == 0) || !(this.jsonPending == 0)) {
+				return;
+			}
+			else if (this.firstTime) {
+				this.init();
+			}
+			else {
+				this.tick();
+			}
+		},
+
+		init: function() {
+			// init game here
+		},
+
+		tick: function() {
+			// tick code hier
+		},
+
+        loadImages: function(fileNames) {
+        	this.images = new Array();
+        	this.imagesPending = fileNames.length;
+        	for(var i = 0, length = fileNames.length; fileName = fileNames[i], i < length; i++) {
+				require(["image!data/" + fileName + '.png'], this.imageLoaded.bind(this, fileName));
+			}
+        },
+
+		imageLoaded: function(fileName, img) {
+			this.imagesPending--;
+			this.images[fileName] = img;
+			//Logger.log('image loaded: ' + fileName + ' - ' + img);
+		},
+
+		loadJson: function(fileNames) {
+			this.json = new Array();
+			this.jsonPending = fileNames.length;
+			for(var i = 0, length = fileNames.length; fileName = fileNames[i], i < length; i++) {
+				//Logger.log(fileName);
+				require(["json!data/" + fileName + '.json'], this.jsonLoaded.bind(this, fileName));
+			}
+		},
+
+		jsonLoaded: function(fileName, json) {
+			this.jsonPending--;
+			this.json[fileName] = json;
+			//Logger.log('json loaded: ' + fileName + ' - ' + json);
+		},
+
+		getCanvas: function() {
+			return this.canvas;
+		}
+	});
+	
+	return Game;
+});
